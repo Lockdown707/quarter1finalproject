6e569133276969a530d5f94a2bfe7770a1ab962d
Lockdown707 <145475291+Lockdown707@users.noreply.github.com>
2023-11-28 08:31:21 -0800

    Button Practice
    Working on Clone trooper and getting uploaded to miximo

---
+++ Assets/Scripts/ButtonTester.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTester : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("You pressed P");
+        }
+
+        if(Input.GetButtonDown("Shoot"))
+        {
+            Debug.Log("You Pressed Shoot");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("You Pressed Jump");
+        }
+    }
+}
